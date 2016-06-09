6b4301233c2800e125d5ea1dbb37b987fd26027c
Dovbo <skorobog@ukr.net>
2016-06-09 15:52:36 +0300

    add parser

---
+++ parser.py
@@ -0,0 +1,54 @@
+from urllib.request import urlopen
+from urllib.parse import urljoin
+
+from lxml.html import fromstring
+
+import csv
+
+import re
+
+URL = 'http://www.0629.com.ua/catalog/41/page/'
+ITEM_PATH = '.conteiner .company_box .info'
+ 
+
+
+def parse_courses():
+	n = 1
+	while n < 17:
+		f = urlopen(URL + str(n))
+		list_html = f.read().decode('utf-8')
+		list_doc = fromstring(list_html)
+
+		for elem in list_doc.cssselect(ITEM_PATH):
+			p = elem.cssselect('p')[0]
+			if p.text is None:
+				continue
+			address = p.text
+			if (not (re.match(u'улица', address) or re.match(u'проспект', address) or 
+				re.match(u'проезд', address) or re.match(u'бульвар', address) or
+				re.match(u'площадь', address) or re.match(u'переулок', address) or
+				re.match(u'квартал', address) or re.match(u'шоссе', address) or
+				re.match(u'пос', address))):
+				# print(address, "\n")
+				
+				a = elem.cssselect('a')[0]
+				text_length = len(a.text) - 3
+				name  = a.text[0:text_length]
+				
+				org = {'name': name, 'address': address}
+			# btn_grey button loader
+				print(org)
+			# else:
+			#	print('!!!!!!!!!!!!!!!!' + address[0:10])
+		n += 1
+
+
+
+
+
+def main():
+	parse_courses()
+
+
+if __name__ == '__main__':
+	main()
