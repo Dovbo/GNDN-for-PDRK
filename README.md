bcf3925d56be9f3901cb2b9c3d37dc19ab33b28c
ds <skorobog@ukr.net>
2016-06-14 08:05:53 +0300

    all city sites add to parser

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# GNDN-for-PDRK
+  GNDN-for-PDRK
+
+For python3.5 env
+
+sudo apt-get install python-dev libxml2-dev libxslt1-dev zlib1
+
+pip install lxml
+pip install cssselect
+
+
--- parser.py
+++ parser.py
@@ -7,41 +7,58 @@ import csv
 
 import re
 
-URL = 'http://www.0629.com.ua/catalog/41/page/'
-ITEM_PATH = '.conteiner .company_box .info'
-
-def parse_courses():
+def parse_city():
 	for n in range(17):
 		f = urlopen(URL + str(n))
 		list_html = f.read().decode('utf-8')
 		list_doc = fromstring(list_html)
-
+		#print(list_doc)
 		for elem in list_doc.cssselect(ITEM_PATH):
 			p = elem.cssselect('p')[0]
 			if p.text is None:
 				continue
 			address = p.text
-			if (not (re.match(u'улица', address) or re.match(u'проспект', address) or 
+			a = elem.cssselect('a')[0]
+			text_length = len(a.text) - 3
+			name  = a.text[0:text_length]
+
+			if ((re.match(u'улица', address) or re.match(u'проспект', address) or 
 				re.match(u'проезд', address) or re.match(u'бульвар', address) or
 				re.match(u'площадь', address) or re.match(u'переулок', address) or
 				re.match(u'квартал', address) or re.match(u'шоссе', address) or
 				re.match(u'пос', address))):
-				# print(address, "\n")
-				
-				a = elem.cssselect('a')[0]
-				text_length = len(a.text) - 3
-				name  = a.text[0:text_length]
-				
+	
 				with open('results.csv', 'a') as f:
-					f.write('%s,%s/n' %(name, address))
-				
-				# org = {'name': name, 'address': address}
-			# btn_grey button loader
-				# print(org)
-			# else:
-			#	print('!!!!!!!!!!!!!!!!' + address[0:10])
+					f.write('%s,%s\n' %(name, address))
+		
+			else:
+				with open('no_address.csv', 'a') as f:
+					f.write('%s --- %s\n' %(name, address)) 
 
+PARENT_URL = 'http://citysites.ua/ua/city/'
+for site in fromstring(urlopen(PARENT_URL).read()).cssselect('a'):
+	current_site = site.cssselect('a')[0].get('href')
+
+	if re.search('\.ua', current_site) != None:
+		print(current_site)
+
+		URL = current_site + '/catalog/41/page/'
+		ITEM_PATH = '.conteiner .company_box .info'
+
+		#city name for possible inserting to output file
+		title = fromstring(urlopen(URL).read().decode('utf-8')).cssselect('title')[0].text.split()
+		if len(title[2]) > 3:
+			city_name = title[2]
+		else:
+			city_name = title[3]
+
+		with open('results.csv', 'a') as f:
+			f.write('\n%s\n\n' %(city_name))
+		with open('no_address.csv', 'a') as f:
+			f.write('\n%s\n\n' %(city_name))
+		print(u'%s processing...\n' %(city_name))
+		parse_city()
 
 
 if __name__ == '__main__':
-	parse_courses()
+	parse_city()
