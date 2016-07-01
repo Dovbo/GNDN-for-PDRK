bf7de2d8551daf1e67ed9434e3623a9b042b6d41
ds <skorobog@ukr.net>
2016-07-01 06:04:06 +0300

    add custom user agent

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen
 from urllib.parse import urljoin
 from lxml.html import fromstring
 
+import urllib.request
 import csv
 import re
 import time
@@ -17,7 +18,8 @@ PHONE_ONLY = 'phone_only.csv'
 
 def parse_city():
 	for n in range(41):
-		f = urlopen(URL + str(n))
+		req = urllib.request.Request(URL + str(n), headers = {'User-Agent' : 'Corruption Tracker'})
+		f = urlopen(req)
 		time.sleep(random.random() / 100)
 		list_html = f.read().decode('utf-8')
 		list_doc = fromstring(list_html)
@@ -30,10 +32,12 @@ def parse_city():
 			a = elem.cssselect('a')[0]
 			text_length = len(a.text) - 3
 			name  = a.text[0:text_length]
+			splitted_address = re.split(',', address)
 			
-			if re.split(',', address)[0] in  [city_name, 'м. ' + city_name, 'г. ' + city_name, 'м.' + city_name, 'г.' + city_name]:
-				address = address[len(city_name) + 2:]
-				print(address)
+			if splitted_address[0] in  [city_name, 'м. ' + city_name,     'г. ' + city_name, 'м.' + city_name, 'г.' + city_name] and re.split(    ',', address)[1] != '':
+				address = splitted_address[1].strip() + ', ' + ','.jo    in(splitted_address[2:])
+			elif len(splitted_address) > 2 and splitted_address[1] ==     '':
+				address = splitted_address[2].strip() + ', ' + ','.jo    in(splitted_address[3:])
 			
 			if re.split(' ', address)[0] in street_types:
 				with open(RESULTS, 'a') as f:
@@ -45,7 +49,8 @@ def parse_city():
 				with open(NO_ADDRESS, 'a') as f:
 					f.write('%s --- %s\n' %(address, name))
 
-for site in fromstring(urlopen(PARENT_URL).read()).cssselect('a'):
+parent_req = urllib.request.Request(PARENT_URL, headers = {'User-Agent' : 'Corruption Tracker'})
+for site in fromstring(urlopen(parent_req).read()).cssselect('a'):
 	current_site = site.cssselect('a')[0].get('href')
 	time.sleep(random.random() / 100)
 
@@ -56,10 +61,18 @@ for site in fromstring(urlopen(PARENT_URL).read()).cssselect('a'):
 		#city name for possible inserting to output file
 		title = fromstring(urlopen(URL).read().decode('utf-8')).cssselect('title')[0].text.split()
 		time.sleep(random.random() / 100)
-		if len(title[2]) > 3:
+		if len(title[2]) > 3 and not title[3][0].isupper():
 			city_name = title[2]
+		elif len(title[2]) > 3 and title [3][0].isupper():
+			city_name = title[2] + ' ' + title[3]
+		elif title[4][0].isupper():
+			city_name = title[3] + ' ' + title[4]
 		else:
 			city_name = title[3]
+		if city_name in ['Тернопiль', 'Тернопіль']:
+			city_name = 'Тернопіль'
+		if city_name == 'Чернівців:':
+			city_name = 'Чернівці'
 
 		with open(RESULTS, 'a') as f:
 			f.write('\n%s\n\n' %(city_name))
